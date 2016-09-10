e5cbd3f359d2af143b5acbee7bd0f3a52d78583b
Drew Seward <awswrd@gmail.com>
2016-09-09 22:08:58 -0700

    Add HttpRequestSender

---
+++ src/Restivus/HttpRequestSender.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restivus
+{
+    public interface IHttpRequestSender
+    {
+        HttpClient HttpClient { get; }
+        Task SendAsync(HttpRequestMessage message);
+        Task<T> SendAsync<T>(HttpRequestMessage message, Func<HttpResponseMessage, Task<T>> deserializeResponseContent);
+        Task<T> SendAsync<T>(HttpRequestMessage message, Func<HttpResponseMessage, T> deserializeResponseContent);
+        Task<T> SendAsync<T>(HttpRequestMessage message, Func<Task<string>, T> deserializeResponseContent);
+        Task<T> SendAsync<T>(HttpRequestMessage message, Func<string, T> deserializeResponseContent);
+    }
+
+    public partial class HttpRequestSender
+    {
+        public static HttpClient DefaultHttpClient { get; } = new HttpClient();
+
+        public static IHttpRequestSender Default { get; } =
+            new HttpRequestSender(DefaultHttpClient);
+    }
+
+    public partial class HttpRequestSender : IHttpRequestSender
+    {
+        public HttpRequestSender(HttpClient client)
+        {
+            HttpClient = client;
+        }
+
+        public HttpClient HttpClient { get; }
+
+        public Task SendAsync(HttpRequestMessage message) => SendAsync<object>(message, _ => null);
+
+        public async Task<T> SendAsync<T>(HttpRequestMessage message, Func<HttpResponseMessage, Task<T>> deserializeResponseContentAsync)
+        {
+            // _Logger.Debug("{message}", message);
+
+            using (message)
+            using (var response = await HttpClient.SendAsync(message))
+            {
+                response.EnsureSuccessStatusCode();
+
+                // _Logger.Debug("{@response}", response);
+
+                return await deserializeResponseContentAsync(response);
+            }
+        }
+
+        public Task<T> SendAsync<T>(HttpRequestMessage message, Func<HttpResponseMessage, T> deserializeResponseContent)
+        {
+            return SendAsync(
+                message,
+                response => Task.FromResult(deserializeResponseContent(response))
+            );
+        }
+
+        public Task<T> SendAsync<T>(HttpRequestMessage message, Func<Task<string>, T> deserializeResponseContent)
+        {
+            return SendAsync(
+                message,
+                response => deserializeResponseContent(response.Content.ReadAsStringAsync())
+            );
+        }
+
+        public Task<T> SendAsync<T>(HttpRequestMessage message, Func<string, T> deserializeResponseContent)
+        {
+            return SendAsync(
+                message,
+                async response =>
+                {
+                    return deserializeResponseContent(await response.Content.ReadAsStringAsync());
+                }
+            );
+        }
+    }
+}
