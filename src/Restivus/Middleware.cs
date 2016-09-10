94630e446d5632a59eaaaf2b8082c311ac168d52
Drew Seward <awswrd@gmail.com>
2016-09-09 22:21:53 -0700

    Add middleware

---
+++ src/Restivus/Middleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restivus
+{
+    public interface IMiddleware<T>
+    {
+        T Run(T thing);
+        Task<T> RunAsync(T thing);
+    }
+
+    public interface IHttpRequestMiddleware : IMiddleware<HttpRequestMessage> { }
+
+    public class HttpRequestMiddleware : IHttpRequestMiddleware
+    {
+        public HttpRequestMessage Run(HttpRequestMessage thing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpRequestMessage> RunAsync(HttpRequestMessage thing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
