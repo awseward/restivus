bcd08b8f618bda8b165f43b8b9fea664f670e78c
Drew Seward <awswrd@gmail.com>
2016-09-09 22:46:54 -0700

    Add IWebApi, IRestClient

---
+++ src/Restivus/RestClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restivus
+{
+    public interface IRestClient
+    {
+        IWebApi WebApi { get; }
+        IReadOnlyCollection<IHttpRequestMiddleware> RequestMiddlewares { get; }
+        IHttpRequestSender RequestSender { get; }
+    }
+
+    public static class RestClientExtensions
+    {
+        public static HttpRequestMessage CreateRequestMessage(this IRestClient client,
+            HttpMethod method,
+            string path)
+        {
+            var message = new HttpRequestMessage(
+                method,
+                client.WebApi.UriForAbsolutePath(path)
+            );
+
+            return client.RequestMiddlewares.Aggregate(
+                message,
+                (msg, middleware) => middleware?.Run(message)
+            );
+        }
+
+        public static Task<T> SendAsync<T>(this IRestClient client,
+            HttpMethod method,
+            string absolutePath,
+            Action<HttpRequestMessage> mutateRequestMessage,
+            Func<HttpResponseMessage, T> deserializeResponse)
+        {
+            var message = client.CreateRequestMessage(method, absolutePath);
+
+            mutateRequestMessage(message);
+
+            return client.RequestSender.SendAsync(message, deserializeResponse);
+        }
+
+        public static Task SendAsync(this IRestClient client,
+            HttpMethod method,
+            string absolutePath,
+            Action<HttpRequestMessage> mutateRequestMessage)
+        {
+            var message = client.CreateRequestMessage(method, absolutePath);
+
+            mutateRequestMessage(message);
+
+            return client.RequestSender.SendAsync(message);
+        }
+    }
+}
---
+++ src/Restivus/WebApi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restivus
+{
+    public interface IWebApi
+    {
+        string Scheme { get; }
+        string Host { get; }
+        int? Port { get; }
+        string BasePath { get; }
+    }
+
+    public static class WebApiExtensions
+    {
+        public static Uri UriForAbsolutePath(this IWebApi webApi, string absolutePath)
+        {
+            return new UriBuilder
+            {
+                Scheme = webApi.Scheme,
+                Host = webApi.Host,
+                Port = webApi.Port ?? -1,
+                Path = absolutePath,
+            }.Uri;
+        }
+
+        public static Uri UriForRelativePath(this IWebApi webApi, string relativePath)
+        {
+            return new UriBuilder
+            {
+                Scheme = webApi.Scheme,
+                Host = webApi.Host,
+                Port = webApi.Port ?? -1,
+                Path = _PathCombine(webApi.BasePath, relativePath),
+            }.Uri;
+        }
+
+        static string _PathCombine(string prefix, string postfix)
+        {
+            return string.Join("/", new[]
+            {
+                prefix?.TrimEnd('/') ?? string.Empty,
+                postfix?.TrimStart('/') ?? string.Empty,
+            });
+        }
+    }
+}
