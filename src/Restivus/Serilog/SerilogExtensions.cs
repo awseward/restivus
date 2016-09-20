12f407ce302dbe304fbb0ee43ecbe621a3db57c8
Drew Seward <awswrd@gmail.com>
2016-09-19 21:39:40 -0700

    Change up the namespace
    From Restivus.Serilog to Serilog.Restivus

--- src/Restivus/Serilog/SerilogExtensions.cs
+++ src/Restivus/Serilog/SerilogExtensions.cs
@@ -1,13 +1,13 @@
-﻿using Serilog;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Restivus;
 
-namespace Restivus.Serilog
+namespace Serilog.Restivus
 {
     public static class SerilogExtensions
     {
