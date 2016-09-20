f74e109915eb11153e5d61e7bc79f3f648271228
Drew Seward <awswrd@gmail.com>
2016-09-19 18:15:27 -0700

    Add Serilog extension for filtering

---
+++ src/Restivus/Serilog/SerilogExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restivus.Serilog
+{
+    public static class SerilogExtensions
+    {
+        public static LoggerConfiguration FilterQueryParamsFromHttpRequestMessage(
+            this LoggerConfiguration loggerConfig,
+            params string[] queryParamKeys)
+        {
+            return loggerConfig.Destructure.ByTransforming<HttpRequestMessage>(
+                msg => new
+                {
+                    msg.Method.Method,
+                    Uri = msg.RequestUri.FilterQueryParams(queryParamKeys),
+                    msg.Headers,
+                }
+            );
+        }
+    }
+}
