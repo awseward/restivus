ee3a061fd25b3071db085324288d8ebea2abd2e8
Drew Seward <awswrd@gmail.com>
2016-09-20 10:04:05 -0700

    [WIP] Next-morning commit
    Mostly committing so I don't lose progress from last night before
    falling asleep.

---
+++ src/Restivus/IHttpRequestPipeline.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Restivus
+{
+    public interface IHttpRequestPipeline
+    {
+        HttpRequestMessage Run();
+        HttpRequestMessage Run(HttpRequestMessage request);
+    }
+
+    public class HttpRequestPipeline : IHttpRequestPipeline
+    {
+        public HttpRequestPipeline(Func<HttpRequestMessage, HttpRequestMessage> run)
+        {
+            _run = run ?? (req => req);
+        }
+
+        readonly Func<HttpRequestMessage, HttpRequestMessage> _run;
+
+        public HttpRequestMessage Run(HttpRequestMessage request) => _run(request);
+
+        public HttpRequestMessage Run() => _run(new HttpRequestMessage());
+    }
+
+    public static class HttpRequestPipelineExtensions
+    {
+        public static Task<HttpResponseMessage> SendAsync(this IHttpRequestPipeline pipeline)
+        {
+            var request = pipeline.Run();
+
+            throw new NotImplementedException();
+        }
+
+        public static Task<HttpResponseMessage> SendAsync<TRequestPayload>(this IHttpRequestPipeline pipeline, TRequestPayload requestPayload)
+        {
+            var request = pipeline.Run();
+
+            throw new NotImplementedException();
+        }
+
+        public static async Task<TResponsePayload> SendAsync<TResponsePayload>(this IHttpRequestPipeline pipeline, Func<HttpResponseMessage, Task<TResponsePayload>> getResponsePayloadAsync)
+        {
+            var request = pipeline.Run();
+
+            // TEMPORARY
+            using (var client = new HttpClient())
+            using (var response = await client.SendAsync(request))
+            {
+                return await getResponsePayloadAsync(response);
+            }
+        }
+
+        public static Task<TResponsePayload> SendAsync<TRequestPayload, TResponsePayload>(this IHttpRequestPipeline pipeline, TRequestPayload payload, Func<HttpResponseMessage, Task<TResponsePayload>> getResponsePayloadAsync)
+        {
+            var request = pipeline.Run();
+
+            throw new NotImplementedException();
+        }
+
+        public static IHttpRequestPipeline Do(this IWebApi webApi, HttpMethod method, string path)
+        {
+            return new HttpRequestPipeline(request =>
+            {
+                request.Method = method;
+                request.RequestUri = webApi.UriForRelativePath(path);
+
+                return request;
+            });
+        }
+        public static IHttpRequestPipeline Get(this IWebApi webApi, string path) => webApi.Do(HttpMethod.Get, path);
+        public static IHttpRequestPipeline Put(this IWebApi webApi, string path) => webApi.Do(HttpMethod.Put, path);
+        public static IHttpRequestPipeline Post(this IWebApi webApi, string path) => webApi.Do(HttpMethod.Post, path);
+
+        public static IHttpRequestPipeline WithQueryParams(this IHttpRequestPipeline pipeline, IDictionary<string, string> queryParams)
+        {
+            return new HttpRequestPipeline(request =>
+            {
+                var transformed = pipeline.Run(request);
+
+                var paramCollection = HttpUtility.ParseQueryString(transformed.RequestUri.Query);
+
+                foreach (var kvp in queryParams)
+                {
+                    paramCollection.Add(kvp.Key, kvp.Value);
+                }
+
+                transformed.RequestUri = new UriBuilder(transformed.RequestUri)
+                {
+                    Query = paramCollection.ToString(),
+                }.Uri;
+
+                return transformed;
+            });
+        }
+
+        public static IHttpRequestPipeline WithQueryParam(this IHttpRequestPipeline pipeline, string name, string value) =>
+            pipeline.WithQueryParams(new Dictionary<string, string> { { name, value } });
+
+        public static IHttpRequestPipeline WithHeaders(this IHttpRequestPipeline pipeline, IDictionary<string, string> headers)
+        {
+            return new HttpRequestPipeline(request =>
+            {
+                var transformed = pipeline.Run(request);
+
+                foreach (var kvp in headers)
+                {
+                    transformed.Headers.Add(kvp.Key, kvp.Value);
+                }
+
+                return transformed;
+            });
+        }
+
+        public static IHttpRequestPipeline WithHeader(this IHttpRequestPipeline pipeline, string name, string value) =>
+            pipeline.WithHeaders(new Dictionary<string, string> { { name, value } });
+    }
+}
---
+++ test/Restivus.Tests/HttpRequestPipelineTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Restivus.Tests
+{
+    public class HttpRequestPipelineTests
+    {
+        public class GitHubWebApi : IWebApi
+        {
+            public string BasePath { get; } = "/";
+            public string Host { get; } = "api.github.com";
+            public int? Port { get; }
+            public string Scheme { get; } = "https";
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        public class GitHubUser
+        {
+            [JsonProperty("id")]
+            int Id { get; set; }
+
+            [JsonProperty("login")]
+            string Login { get; set; }
+
+            [JsonProperty("followers_url")]
+            string FollowersUrl { get; set; }
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        public class GitHubUserSearchResults
+        {
+            [JsonProperty("total_count")]
+            int TotalCount { get; set; }
+
+            [JsonProperty("incomplete_results")]
+            bool IncompleteResults { get; set; }
+
+            [JsonProperty("items")]
+            List<GitHubUser> Users { get; set; }
+        }
+
+        [Fact]
+        public async Task PipelineApproachWorks()
+        {
+            var results = await new GitHubWebApi()
+                .Get("/search/users")
+                .WithQueryParam("q", "dude")
+                .WithQueryParam("per_page", "100")
+                .WithHeader("User-Agent", "Pipelines/1.0")
+                .SendAsync(
+                    async response =>
+                    {
+                        response.EnsureSuccessStatusCode();
+
+                        return JsonConvert.DeserializeObject<GitHubUserSearchResults>(
+                            await response.Content.ReadAsStringAsync()
+                        );
+                    }
+                );
+
+            //var request = findMe.Run();
+
+            //Assert.Equal(
+            //    new Uri("https://api.github.com/search/users?q=awseward"),
+            //    request.RequestUri
+            //);
+
+            //Assert.Equal(
+            //    "world",
+            //    request.Headers.GetValues("X-Hello").Single()
+            //);
+        }
+    }
+}
