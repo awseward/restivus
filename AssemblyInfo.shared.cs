1eff1d151dd878017075835c069239a7554f3e4d
Drew Seward <awswrd@gmail.com>
2017-10-07 13:07:38 -0700

    version: Increment patch -> 0.7.1

--- AssemblyInfo.shared.cs
+++ AssemblyInfo.shared.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.7.1.0")]
 [assembly: AssemblyFileVersion("0.7.1.0")]
-[assembly: AssemblyInformationalVersion("0.7.1-a")]
+[assembly: AssemblyInformationalVersion("0.7.1")]
