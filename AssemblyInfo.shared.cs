03b348cc7ff1589fd2b7c0ab939f9ab2d1b5b1af
Drew Seward <awswrd@gmail.com>
2018-03-09 19:12:22 -0800

    version: Pre-increment patch -> 0.8.1-a

--- AssemblyInfo.shared.cs
+++ AssemblyInfo.shared.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
-[assembly: AssemblyInformationalVersion("0.8.0")]
+[assembly: AssemblyVersion("0.8.1.0")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyInformationalVersion("0.8.1-a")]
